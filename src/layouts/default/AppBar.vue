10a34bce427b2de3a507d7d0df0f20b4780068e6
Wern Ancheta <ancheta.wern@gmail.com>
2024-04-28 16:35:30 +0800

    [improvement] add login and homepage links

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,15 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
+      <a href="/" style="color: #333;">
       <v-icon icon="mdi-food" />
       PinoyFood
+      </a>
     </v-app-bar-title>
+
+    <div class="mr-2">
+      <a href="/login" style="color: #333;">login</a>
+    </div>
   </v-app-bar>
 </template>
 
