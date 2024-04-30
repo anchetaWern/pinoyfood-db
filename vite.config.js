25ffe8d311c6e7fd07d582ce033d55354ace4cbe
Wern Ancheta <ancheta.wern@gmail.com>
2024-04-30 20:10:52 +0800

    [feature] implement pwa

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ 
+      
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,eot,woff,woff2}']
+      },
+      devOptions: {
+        enabled: true
+      },
+
+      manifest: {
+        name: 'PinoyFood DB',
+        short_name: 'pinoyfood',
+        description: 'View nutrition information of Filipino foods',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
