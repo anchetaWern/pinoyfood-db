06d5fedb8ea39b0a06145dda2649f5bdda932286
Wern Ancheta <ancheta.wern@gmail.com>
2025-11-03 13:22:23 +0800

    [revision] 1500px

--- src/views/BulkImportView.vue
+++ src/views/BulkImportView.vue
@@ -77,7 +77,7 @@ export default {
       return new Promise((resolve, reject) => {
         new Compressor(blob, {
           quality: 0.8,
-          width: 2000,
+          width: 1500,
 
           success(blob_obj) {
             const reader = new FileReader();
